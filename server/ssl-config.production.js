202f17ea045c7f5164a556070c4b0065069a93f3
dasEinhorn1 <achayward1@gmail.com>
2018-08-07 15:34:42 -0400

    preparing for deployment on server

---
+++ server/ssl-config.production.js
@@ -0,0 +1,11 @@
+
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-example-ssl
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var path = require('path');
+var fs = require('fs');
+
+// exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privatekey.pem')).toString();
+// exports.certificate = fs.readFileSync(path.join(__dirname, './private/certificate.pem')).toString();
