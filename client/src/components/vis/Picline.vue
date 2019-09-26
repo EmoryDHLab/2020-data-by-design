954c6a68c3f7a139f30d1ca00237e0681ee171d8
Jianing Fu <jianingfu@gatech.edu>
2019-09-25 23:56:48 -0400

    added pic, formatted text to timeline, working on frame pos x for points from the same year

--- client/src/components/vis/Picline.vue
+++ client/src/components/vis/Picline.vue
@@ -3,27 +3,26 @@
     :width="styles.width + styles.margin.left + styles.margin.right + 'px'"
     :height="styles.height + styles.margin.top + styles.margin.bottom + 'px'">
     <g :transform="`translate(${styles.margin.left}, ${styles.margin.top})`">
+
+      <!--gray bar-->
       <rect id="timeline"
         x='0' :y='(styles.height - styles.line.height)/2'
         :height="styles.line.height"
         :width="styles.line.width"
         fill="#989898"
         rx="6px" ry="6px"></rect>
+
       <g v-for="yearPoints in groupedByYear"
-        :style = positionYear(yearPoints)>
-        <g :transform="`translate(-22,${-positionYearY(yearPoints)+(styles.height+styles.margin.top + styles.margin.bottom)/2})
-          ,scale(0.008,-0.008)`"
-           fill="#989898" stroke="white" stroke-width="50">
-          <path d="M3212 5793 c-18 -9 -44 -28 -57 -42 -12 -14 -61 -69 -106 -121 -46
--52 -86 -97 -89 -100 -3 -3 -34 -39 -70 -80 -36 -41 -85 -96 -109 -122 l-44
--48 -928 -2 -929 -3 -27 -28 -28 -27 0 -1410 0 -1410 28 -27 27 -28 2450 0
-2450 0 27 28 28 27 0 1410 0 1410 -28 27 -27 28 -959 3 -958 2 -44 48 c-24 26
--73 81 -109 122 -36 41 -67 77 -70 80 -3 3 -43 48 -89 100 -45 52 -94 107
--107 121 -51 57 -161 77 -232 42z"/>
-        </g>
-        <g :style="`transform: translate(-22,${-positionYearY(yearPoints)+(styles.height+styles.margin.top + styles.margin.bottom)/2}`">
-          <image xlink:href="yearPoints.img" width="10%" height="10%"/>
-        </g>
+         :style = positionYear(yearPoints)>
+        <!--blue bars-->
+        <rect
+                @mouseenter="$emit('focus-image', yearPoints.points[0].id)"
+                :width="styles.line.height"
+                :height="scaleY(yearPoints.count)"
+                fill="#4A90E2"
+                :rx="styles.line.height/2" :ry="styles.line.height/2"></rect>
+
+        <!--year text-->
         <text
           v-if="yearPoints.year % 2 == 0"
           transform="rotate(45), translate(-40, 0)"
@@ -34,12 +33,39 @@
             translate(-${(55 + scaleY(yearPoints.count)) * 0.7071},
             ${(scaleY(yearPoints.count) + 15) * 0.7071})`"
           style="font-size:14px">{{yearPoints.year}}</text>
-        <rect
-          @mouseenter="$emit('focus-image', yearPoints.points[0].id)"
-          :width="styles.line.height"
-          :height="scaleY(yearPoints.count)"
-          fill="#4A90E2"
-          :rx="styles.line.height/2" :ry="styles.line.height/2"></rect>
+      </g>
+
+      <g v-for="point in dataset" :style = positionPointX(point)>
+        <!--frames-->
+        <g v-if="point.year % 2 == 0"
+           :transform="`translate(${-22},${(styles.height+styles.margin.top + styles.margin.bottom + styles.framepadding)/2})
+          ,scale(0.008,-0.008)`"
+           fill="#989898" stroke="white" stroke-width="50">
+          <path d="M3212 5793 c-18 -9 -44 -28 -57 -42 -12 -14 -61 -69 -106 -121 -46
+-52 -86 -97 -89 -100 -3 -3 -34 -39 -70 -80 -36 -41 -85 -96 -109 -122 l-44
+-48 -928 -2 -929 -3 -27 -28 -28 -27 0 -1410 0 -1410 28 -27 27 -28 2450 0
+2450 0 27 28 28 27 0 1410 0 1410 -28 27 -27 28 -959 3 -958 2 -44 48 c-24 26
+-73 81 -109 122 -36 41 -67 77 -70 80 -3 3 -43 48 -89 100 -45 52 -94 107
+-107 121 -51 57 -161 77 -232 42z"/>
+        </g>
+
+        <g v-if="point.year % 2 != 0"
+           :transform="`translate(${22},${(styles.margin.top + styles.margin.bottom - styles.framepadding)/2})
+          ,scale(0.008,-0.008), rotate(180)`"
+           fill="#989898" stroke="white" stroke-width="50">
+          <path d="M3212 5793 c-18 -9 -44 -28 -57 -42 -12 -14 -61 -69 -106 -121 -46
+-52 -86 -97 -89 -100 -3 -3 -34 -39 -70 -80 -36 -41 -85 -96 -109 -122 l-44
+-48 -928 -2 -929 -3 -27 -28 -28 -27 0 -1410 0 -1410 28 -27 27 -28 2450 0
+2450 0 27 28 28 27 0 1410 0 1410 -28 27 -27 28 -959 3 -958 2 -44 48 c-24 26
+-73 81 -109 122 -36 41 -67 77 -70 80 -3 3 -43 48 -89 100 -45 52 -94 107
+-107 121 -51 57 -161 77 -232 42z"/>
+        </g>
+
+        <!--images-->
+        <g :style="`transform: translate(-22,${(styles.height+styles.margin.top + styles.margin.bottom)/2}`">
+          <image xlink:href="yearPoints.img" width="10%" height="10%"/>
+        </g>
+
       </g>
     </g>
   </svg>
@@ -64,6 +90,7 @@ const DEFAULT_OPTIONS = {
   styles: {
     height: 200,
     width: 1000,
+    framepadding: 10,
     margin: {
       top: 50,
       right: 50,
@@ -153,6 +180,10 @@ export default {
       const dy = (this.styles.height - this.scaleY(yearPoints.count))/2
       return dy
     },
+    positionPointX (point) {
+        const dx = this.scaleX(point.year) - this.styles.line.height/2
+        return `transform: translate(${dx}px, 0px);`
+    }
   }
 }
 </script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,4 +1,4 @@
-<template lang="html">
+<template lang="html" xmlns:https="http://www.w3.org/1999/xhtml">
   <div class="">
     <main>
       <div id="home" class="header">
@@ -11,13 +11,13 @@
           <h3 class="subtitle" style="font-size: 1em">What is the story we tell about the emergence of modern data visualization?
             <br>How might we tell that story differently?
           </h3>
-          <div class="reading-progress">
-            <button class="--uppercase btn"
-              style="margin-right: 40px;"
-              type="button"
-              name="start-reading">Resume reading</button>
-            <ProgressBar :range="[0, 160]" :value="155"/>
-          </div>
+          <!--<div class="reading-progress">-->
+            <!--<button class="&#45;&#45;uppercase btn"-->
+              <!--style="margin-right: 40px;"-->
+              <!--type="button"-->
+              <!--name="start-reading">Resume reading</button>-->
+            <!--<ProgressBar :range="[0, 160]" :value="155"/>-->
+          <!--</div>-->
         </div>
         <Picline :dataset="points" id="picline"/>
       </div>
@@ -28,7 +28,11 @@
           <br>the second line sine wim laenme
         </h3>
         <div class="chapter-links">
-          <div class="card">
+          <div class="card"
+               style="background-image: url(https://cdn.theatlantic.com/assets/media/img/mt/2015/03/RossBetsy_1/lead_720_405.jpg?mod=1533691734);
+               background-size: contain;
+               background-color: rgba(255, 255, 255, 0.6);
+               background-blend-mode: lighten;">
             <h2 class="boxHeading">A View “Simple and Complete”</h2>
             <h3 style="font-size: 1em; margin-top: 0em">William Playfair and the Origins of Objective Display</h3>
           </div>
@@ -79,7 +83,7 @@
         <h3 class="subtitle subclass">These features are still under development
           <br> but feel free to look around!</h3>
         <div class="feature-list">
-          <FeatureCard :largeMode="true" imageLink="https://uce8a584ed8d4e5e49dd5cadfe05.previews.dropboxusercontent.com/p/thumb/AAjwJxNft1znw8Qb7E8jqJNZm50khXD4znjpn-190x8UtRmF-wl6w_CxE5AspPtzgPr1wcZ_9YOyxvI-9_lXF8gSsHUxUgs7I18EGY1ojWEhispqeuWVm23SnptWCIRzPDXy1cWnEhObIVhNv0QdG19XecW0rGZ6g6j-i9CQ5JPKh6Vgq6hX-FpIfb2cOkLE3ZdotdhC0hNP4zdJ4eYkOopwtPytfl1uZ2YMJGhUQirw-KFp-NzFRHsoENbLRSoE_CXS1NrVM-cr7680a5aUt-u3e6gD6mHhtKGZuM-UDETWNqZwT4_PUR1WumAjN2MGF-k6uTkEXYUQUgpTiQfANWVQ/p.jpeg?fv_content=true&size_mode=5">
+          <FeatureCard :largeMode="true" imageLink="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_660,h_382/https://discoverpods.com/wp-content/uploads/2018/12/maps-atlantic-oldtimer-car-compass-vintage-1442539-pxhere.com_-660x382.jpg">
             <template slot="title">Take notes while you read</template>
             Take note while reading, and access them whenever.
           </FeatureCard>
@@ -167,257 +171,249 @@ export default {
     points: [
       {
         id: 0,
-        img: "https://uce8a584ed8d4e5e49dd5cadfe05.previews.dropboxusercontent.com/p/thumb/AAjwJxNft1znw8Qb7E8jqJNZm50khXD4znjpn-190x8UtRmF-wl6w_CxE5AspPtzgPr1wcZ_9YOyxvI-9_lXF8gSsHUxUgs7I18EGY1ojWEhispqeuWVm23SnptWCIRzPDXy1cWnEhObIVhNv0QdG19XecW0rGZ6g6j-i9CQ5JPKh6Vgq6hX-FpIfb2cOkLE3ZdotdhC0hNP4zdJ4eYkOopwtPytfl1uZ2YMJGhUQirw-KFp-NzFRHsoENbLRSoE_CXS1NrVM-cr7680a5aUt-u3e6gD6mHhtKGZuM-UDETWNqZwT4_PUR1WumAjN2MGF-k6uTkEXYUQUgpTiQfANWVQ/p.jpeg?fv_content=true&size_mode=5",
-        year: 1786
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1786,
+        yearCount: 0
       },
       {
         id: 1,
         img: "https://uc995b0d3c37d4bfa812b91ad750.previews.dropboxusercontent.com/p/thumb/AAgR5yIdwNc4zbmzegvln_ZnGQs4xUjgbOhYi2OoAue_u12wQaqNoIWOx_vWZD7y7vzq2vyJeKovtRo4rjMFq7AjR7cv0EZn5XbBdvNBtOrN4CzrKPh2ENk7o94wRo2fcR8pwZt0EhpAuY0Nm8d85-E4gSucP-B99D3y9HFaz-um_lhe5ICTCXNrT2KMeSuqcOXYqPYoCH-chrdjUWxAYIxfQcuVZ57W_jd7TFXrIMgNWgcu2bTl267njzmH8O5RcN0YH8TD4c8dtrNEuJOg-Jtx0AM5-w7AeGPaLE2i-jpc4H9TujjNSb0AqJFsHT6sFoCGFSUKUAx1UqAznJNqqZJW/p.jpeg?fv_content=true&size_mode=5",
-        year: 1786
+        year: 1786,
+        yearCount: 1
       },
       {
         id: 2,
         img: "https://uc882606e8a4bb3797527b681f1a.previews.dropboxusercontent.com/p/thumb/AAglwYWY-yUVAgjCxFx80hCRkKVr4iZ36XnkKTJE1ASABGaCu27JD5Oy7zIlwanyvvVFrj5KKWSPRFfGKYjo6C_qcb0qDPzF4qn4d25_hjULbqSh8BXtI_qtoDf1v_KFlcybcks0wvrMYx7q94A7OiqPRtrnPbKOiJTe1ErzDc0O5O8e1upUl22mgk3UBGk0hMrRsz2G1Mg-C-VfRk03WxKILxQgN8zTkRflTZiqNHYiyyyUWW5lRJtM_QzYrBQeJRAhyvjPp1eo6nH51gOxgw_7I-HruFthFQ_TQcmLt7rJwo-Kw7HF3nk7kx1Grv2ebJPd8EQk0iSYa7IYF7pv4Fm4/p.jpeg?fv_content=true&size_mode=5",
-        year: 1786
+        year: 1786,
+        yearCount: 2
       },
       {
         id: 3,
         img: "https://ucc66bb22d065a3bc0090662b069.previews.dropboxusercontent.com/p/thumb/AAieGA6hORzPvalZgg1-6-R8dRTTpL8UdKjQsJgBvy_s_7ZlJ0xz954xov9YSNWliIl4ed6TIFovu_PEglzF62sSzoG1eM4TBTKIQpdDp9VX41yxg1wzCE31fEU0UA_S1zqieviKxf3C-Drldo0VZxdQnW-lGagzodT3wj0ZZueAj65WiV6PKLRNBOMFxHIEnImHmLT2vlUgrLPuw5YSRc4UNTVaSasKoEHCxQsePbNuod4z1GI4auLzzZb_inSNCQGQaACOXH235rU4-oPzb-vtvTv3hl32UnnRo5DDFtxCQm76hgQTWYLd34Xes3ci_sLSebsk7KTXWFSFapLOUzGQ/p.jpeg?fv_content=true&size_mode=5",
-        year: 1788
+        year: 1788,
+          yearCount: 0
       },
       {
         id: 4,
         img: "https://ucd1608216c56b754036d48a86ca.previews.dropboxusercontent.com/p/thumb/AAhd3P3kgaN3evK2w1mihVyQRfg9vRsNOmPpn0Tnn7ON37DK2fu-WW51xlLhhCFusMIZtf8fwSMTMZII_x4aax11F_2EZf2xtjvCYuPX-Z1L3i14qb01c0UMCJ-F5twnAC8QFdfmVk3fc_grtE0enR0RjiC0M3_N0GhYAzocpRemmW6xZpDw7d-QMlwm9GpL4IEiG73cJwsjGJr_SLd0WxdxIHTK9HgQ0rPAxT9NJxWdO1LxMCDLZAaTNXKr3EweFhgmHsp17Lf_BxjxCkY051wBf9BtRGrYhMmWg7PsKcTlQjbEOBEeOcsXtdYyZ8nspg9RoqkvD067nuvZ1YnyKnsE5p5JbrUHQmKxIbBLG6wyiVFkOxKMDlg9XrJjrVU0A2hbMReqp5ElcJq0E7eRXSbAcDb-Bb9SXXPoZwXUzP4oKD57N6-MgPEHPnoZmWv0weRjgX7FBj_79CsDaPrZ5VkV/p.jpeg?fv_content=true&size_mode=5",
-        year: 1801
+        year: 1801,
+          yearCount: 0
       },
       {
         id: 5,
         img: "https://uce1c965a3cec4b2f32279ff4ecb.previews.dropboxusercontent.com/p/thumb/AAgg-FtoE5I_mWprWikt6tEesWLs9wcAoM6W6WaJDs374NqcDpQnPYuhCkdW-td57F-PYZcvspTldMr4H_4N-tm3r5dkn--LzM19h9h0zYma3tc7dzqby2K7q-sLf9lg1LGfnYSnRXe5myAI-8ZHWCZs2a33JZeYkbKQMrHyTS11jB1DWST3FcHdu3JVG9Qcdv_F4Slzd4l2vjZlBAwxlR9AuV8uUbAHEL1JL0apK0rDoe1WLLmiph3eIZ9b8moxviW8zfcY1g0kx2UkiDgnknTmc1gWVJbMEFQGiDGk_4Tvr9NhvS6LfOrDSiPKKRG3LWP0oMttNcr9eHvw3GhEc2bp/p.jpeg?fv_content=true&size_mode=5",
-        year: 1801
+        year: 1801,
+          yearCount: 1
       },
       {
         id: 6,
         img: "https://uccde6293d9e7799f13c8c0931c6.previews.dropboxusercontent.com/p/thumb/AAiK2PRxAkDNvjpD02B6SopaFyspcPvu-hAHcZq72JAvcqMcLyVHpcjH4l9nzoK_CsqbKNPl-0LR6u0gSU0_IsL6p9swam1DnQlxmwX9oavJnoGgWxiUDsY0Bz2XxujQt0neGYKRIbkBs9K0IzKQWYyOdvNw9eSKcyUOdpanH5RrIEEJzA_imPByERwiGdiS-s340GqZNQyeK2NqYLhGc0Z0uU9Vqh3Sx6QZh_B4XhVSkdYlyFKYjnkDY_T2FY_KlAnr0RKRQk46kTcwez0vCHoKxuLjQXaZWxClsG_av14mCMmCOhT0vOcs47jIGY4g40N-ZeUEboBnZeDfVv6YQu_xSPRHb0ETHUI7WfN-dutyuB0ZtxkUuERn6CXUau-FcjhUaqlmZhcUkJ57IKfZiHqYsElI2nxMVKG_ZxfJp6NwzwUpAzIDlaHJ7FHUL9fh9MGV2vs_CRpWbxdGzRiQz9R-/p.jpeg?fv_content=true&size_mode=5",
-        year: 1801
+        year: 1801,
+          yearCount: 2
       },
       {
         id: 7,
         img: "https://uc114ee4c8621182fac32c03498d.previews.dropboxusercontent.com/p/thumb/AAhtYlC2ctvHeF1A2AofumMUkH0DLar4JOEiK1tHOQCTKU5SnOyJvxwwGgkbp1k79ruf6IA7c5pwqD3YYeuty0d_pb3H9dSY9iYvZB43Mpz1YMPgV8P8RhL3ktTCknZ0k62aUSXyvKghUzcxW-Y9dGZL5a9apX_mzatzDuO7Iv6t_9TDshPtI21T2BOgQ7DW3_Ave4PSv59j4Kkf01W7rRUjZ0GTU0L2xFAqy9bIUlZRlk6KHkGwKu5PT8y--e1uWL-4q3QrlMOruxySAO7GVYAuFpXxafvB-AJzXCyqp_u3GIVuRFZwkUStEtZs33ZQAPOkqp4BOGAy4I5NEbIP0X-EmaOcQewchESdPgK32bFGfIZEl9DRM_CpfnwZHCXNxsU2WIffSwtcDPRs_JPCSpJdhaeQVyb36d9MK35Yzm2NYlSJtJHDj6nZpbZRj91IT_Idb90puq9Y2wBaqwB8_USD/p.png?fv_content=true&size_mode=5",
-        year: 1801
+        year: 1801,
+          yearCount: 3
       },
       {
         id: 8,
         img: "https://uc3c59dcf5e18a7c0b08be33ac6a.previews.dropboxusercontent.com/p/thumb/AAiRal8bJyopaRe34epoYwWq-PXMho69D7M9cMsuIZqpEc4h5IyKlfaQbEZ514wRTyrW0j8Uvh3J9wWKmdBeaVAO5Iqw2NJDxlzmz2NzD2evx9ftOVljHia3xRS_2MUp_XAW0HyB7NKZNAl3LksQGux1P6WnZxwR5OSo-ylsOsiTId785uTIMrMjjIKT5VmKUyrE7DrXRGzWIzPu0r9NkSLxlE3_T5P3g2f0Z2FvU93XxkRg9nm_Sd_UorFEBwOmYGdey8vPd6xPU3vSu3nAOb_GE88feBttExdJWSb8zUk9lleuTJqiEpWgfnenQZ_Prl1IImxEkuFJJ0IU2XE1TVUgagmwSULSXazS4WeIa7jbWQrd8O_0hK_iCWqdyjq17gF6BzbKM1er4-wbHZ3yZBbsFE_dtvv9y8Zu7nlwEaE2AQmdmQChJXdkIB1JVCDFiMKN-vwzjs9oo6oMuPT2iYvB/p.jpeg?fv_content=true&size_mode=5",
-        year: 1801
+        year: 1801,
+          yearCount: 4
       },
       {
         id: 9,
-        img: "blah/blah",
-        year: 1801
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1801,
+          yearCount: 5
       },
       {
         id: 10,
-        img: "blah/blah",
-        year: 1821
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1821,
+          yearCount: 0
       },
       {
         id: 11,
-        img: "blah/blah",
-        year: 1821
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1821,
+          yearCount: 1
       },
       {
         id: 12,
-        img: "bleh/bleh",
-        year: 1858
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1858,
+          yearCount: 0
       },
       {
         id: 13,
-        img: "bleh/bleh",
-        year: 1829
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1829,
+          yearCount: 0
       },
       {
         id: 14,
-        img: "bleh/bleh",
-        year: 1829
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1829,
+          yearCount: 1
       },
       {
         id: 15,
-        img: "bleh/bleh",
-        year: 1829
+        img: "https://www.whitman.edu/assets/images/Academics/Body-Image/history.jpg",
+        year: 1829,
+          yearCount: 2
       },
       {
         id: 16,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 3
       },
       {
         id: 17,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 4
       },
       {
         id: 18,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 5
       },
       {
         id: 19,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 6
       },
       {
         id: 20,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 7
       },
       {
         id: 21,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 8
       },
       {
         id: 22,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 9
       },
       {
         id: 23,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 10
       },
       {
         id: 24,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 11
       },
       {
         id: 25,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 12
       },
       {
         id: 26,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 13
       },
       {
         id: 27,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 14
       },
       {
         id: 28,
         img: "bleh/bleh",
-        year: 1829
+        year: 1829,
+          yearCount: 15
       },
       {
         id: 29,
         img: "bleh/blrh",
-        year: 1846
+        year: 1846,
+          yearCount: 0
       },
       {
         id: 30,
         img: "bleh/blrh",
-        year: 1856
+        year: 1856,
+          yearCount: 0
       },
       {
         id: 31,
         img: "bleh/blrh",
-        year: 1856
+        year: 1856,
+          yearCount: 1
       },
       {
         id: 32,
         img: "bleh/blrh",
-        year: 1856
+        year: 1856,
+          yearCount: 2
       },
       {
         id: 33,
         img: "bleh/blrh",
-        year: 1856
+        year: 1856,
+        yearCount: 3
       },
       {
         id: 34,
         img: "bloo/bloo",
-        year: 1900
+        year: 1900,
+        yearCount: 0
       },
       {
         id: 35,
         img: "bloo/bloo",
-        year: 1900
+        year: 1900,
+          yearCount: 1
       },
       {
         id: 36,
         img: "bloo/bloo",
-        year: 1900
+        year: 1900,
+          yearCount: 2
       },
       {
         id: 37,
         img: "bloo/bloo",
-        year: 1900
+        year: 1900,
+          yearCount: 3
       },
       {
         id: 38,
         img: "bloo/bloo",
-        year: 1900
+        year: 1900,
+          yearCount: 4
       },
       {
         id: 39,
         img: "bloo/bloo",
-        year: 1900
+        year: 1900,
+          yearCount: 5
       },
       {
         id: 40,
         img: "bloo/bloo",
-        year: 1900
-      },
-      {
-
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
-      },
-      {
-        img: "bloo/bloo",
-        year: 1900
+        year: 1900,
+          yearCount: 6
       },
     ].reduce((obj, item, index) => ({
       ...obj,
