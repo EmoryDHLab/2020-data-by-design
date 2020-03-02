63bea341633d5facc5f9d4ea9d5f097d50a8b32e
Dan Jutan <danjutan@gmail.com>
2020-03-02 11:51:16 -0500

    create notebook vue components

---
+++ client/src/components/notebook-new/Notebook.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="notebook-container">
+    <div class="notebook-header">
+      <h2>Current Chapter Title</h2>
+    </div>
+    <div class="notebook-body">
+      <NodeList></NodeList>
+    </div>
+  </div>
+</template>
+
+<script>
+import NodeList from "./NotebookNodeList"
+export default {
+  components: {
+    NodeList
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/notebook-new/NotebookNodeList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="drag-target">
+    <ul>
+      
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import ListItem from "./NotebookNodeListItem"
+export default {
+  components: {
+    ListItem
+  }
+}
+</script>
