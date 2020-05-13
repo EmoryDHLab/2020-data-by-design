745f0c45f7a9cbe38bb46d05b5b608d875cc26bb
Dan Jutan <danjutan@gmail.com>
2020-05-13 14:34:56 -0400

    remove overflow neighbors; don't freak out when we remove everything at once

--- client/src/mixins/Highlightable.js
+++ client/src/mixins/Highlightable.js
@@ -6,9 +6,20 @@ const highlightClass = "nb-user-highlight";
 const overflowNextClass = "nb-overflow-next";
 const overflowPrevClass = "nb-overflow-prev";
 
+const blockContainer = node => {
+  if (!node) return;
+  if (node.nodeType === Node.ELEMENT_NODE) {
+    const style = getComputedStyle(node, null);
+    if (style.display === "block") {
+      return node;
+    }
+  }
+  return blockContainer(node.parentElement);
+}
+
 function Highlightable(rootElementSelector) {
   return {
-    created: function() {
+    created: function () {
       this.log("Highlightable Mixin Initialized");
     },
     data: function () {
@@ -17,7 +28,7 @@ function Highlightable(rootElementSelector) {
         contextMenu: null
       }
     },
-    mounted: function() {
+    mounted: function () {
       let el = rootElementSelector ? document.querySelector(rootElementSelector) : this.$el;
       if (!el) {
         console.error("Highlightable mixin doesn't see any HTML element. Be sure to pass in an HTML Element to the Highlightable(element) constructor.");
@@ -29,13 +40,13 @@ function Highlightable(rootElementSelector) {
       ...mapGetters(["isLoggedIn"])
     },
     watch: {
-      isLoggedIn (isLoggedIn) {
+      isLoggedIn(isLoggedIn) {
         if (isLoggedIn) {
           const notebook = this.$store.getters.notebook;
           notebook.map(obj => obj.metadata)
-                  .filter(metadata => !metadata.startsWith("usernote"))
-                  .map(metadata => this.deserializeRange(metadata))
-                  .forEach(this.createHighlightFromRange);
+            .filter(metadata => !metadata.startsWith("usernote"))
+            .map(metadata => this.deserializeRange(metadata))
+            .forEach(this.createHighlightFromRange);
         } else {
           this.removeAllHighlights();
         }
@@ -47,7 +58,7 @@ function Highlightable(rootElementSelector) {
       },
       onMouseUp(event) {
         if (event.button !== 0) {
-            return;
+          return;
         }
         const selection = window.getSelection();
         if (selection.rangeCount === 0) {
@@ -83,18 +94,9 @@ function Highlightable(rootElementSelector) {
         element.style.top = totalHeight + "px";
       },
       createHighlightFromRange(range) {
-        const parent = node => {
-          if (node.nodeType === Node.ELEMENT_NODE) {
-            const style = getComputedStyle(node, null);
-            console.log(style);
-            if (style.display === "block") {
-              return node;
-            }
-          }
-          return parent(node.parentElement);
-        }
-        const startParent = parent(range.startContainer);
-        const endParent = parent(range.endContainer);
+
+        const startParent = blockContainer(range.startContainer);
+        const endParent = blockContainer(range.endContainer);
         const sameParent = startParent.isEqualNode(endParent);
         const rangeData = this.serializeRange(range);
 
@@ -116,12 +118,12 @@ function Highlightable(rootElementSelector) {
             endParent.prepend(lastSection);
             if (contents.childElementCount > 2) {
               Array.from(contents.childNodes)
-              .slice(1,contents.childNodes.length - 1)
-              .forEach(element => {
-                const section = this.createHighlight(element);
-                section.classList.add(overflowPrevClass, overflowNextClass);
-                startParent.after(section);
-              });
+                .slice(1, contents.childNodes.length - 1)
+                .forEach(element => {
+                  const section = this.createHighlight(element);
+                  section.classList.add(overflowPrevClass, overflowNextClass);
+                  startParent.after(section);
+                });
             }
           }
         }
@@ -130,40 +132,82 @@ function Highlightable(rootElementSelector) {
         console.log("Got here");
         document.querySelectorAll(`.${highlightClass}`).forEach(this.removeHighlightSpan);
       },
-      removeHighlightSpan (span) {
-        const prevSibling = span.prevSibling;
-        const nextSibling = span.nextSibling;
-        const firstChild = span.firstChild;
-        const lastChild = span.lastChild;
+      removeHighlightSpan(span) {
+        console.log(span);
+        if (!span) {
+          console.error("Bad span!")
+          return;
+        }
+        const classList = span.classList;
+        if (!(classList && classList.contains(highlightClass))) {
+          return;
+        }
 
-        const tag = elem => elem.outerHTML.slice(0, elem.outerHTML.indexOf(elem.innerHTML));
+        const alsoRemove = [];
 
-        if (prevSibling && firstChild
-          && prevSibling.nodeType === Node.ELEMENT_NODE 
-          && firstChild.nodeType === Node.ELEMENT_NODE 
-          && tag(prevSibling) === tag(firstChild)) {
+        if (classList.contains(overflowNextClass)) {
+          const block = blockContainer(span);
+          if (block) {
+            const nextHighlight =
+              block.nextElementSibling.classList.contains(highlightClass) ?
+                block.nextElementSibling : block.nextElementSibling.firstElementChild;
+            if (nextHighlight && nextHighlight.classList.contains(highlightClass)) {
+              alsoRemove.push(nextHighlight)
+            }
+          }
+        }
+
+        if (classList.contains(overflowPrevClass)) {
+          const block = blockContainer(span);
+          if (block) {
+            const prevHighlight =
+              block.previousElementSibling.classList.contains(highlightClass) ?
+                block.previousElementSibling : block.previousElementSibling.lastElementChild;
+            if (prevHighlight && prevHighlight.classList.contains(highlightClass)) {
+              alsoRemove.push(prevHighlight);
+            }
+          }
+        }
+
+        if (blockContainer(span) == span) {
+          span.classList.remove(highlightClass, overflowNextClass, overflowPrevClass);
+          delete span.dataset.rangeData;
+        } else if (span.parentNode) {
+          const prevSibling = span.prevSibling;
+          const nextSibling = span.nextSibling;
+          const firstChild = span.firstChild;
+          const lastChild = span.lastChild;
+
+          const tag = elem => elem.outerHTML.slice(0, elem.outerHTML.indexOf(elem.innerHTML));
+
+          if (prevSibling && firstChild
+            && prevSibling.nodeType === Node.ELEMENT_NODE
+            && firstChild.nodeType === Node.ELEMENT_NODE
+            && tag(prevSibling) === tag(firstChild)) {
             prevSibling.innerHTML += firstChild.innerHTML;
             firstChild.remove();
-        }
+          }
 
-        if (nextSibling && lastChild
-          && nextSibling.nodeType === Node.ELEMENT_NODE 
-          && lastChild.nodeType === Node.ELEMENT_NODE 
-          && tag(nextSibling) === tag(lastChild)) {
+          if (nextSibling && lastChild
+            && nextSibling.nodeType === Node.ELEMENT_NODE
+            && lastChild.nodeType === Node.ELEMENT_NODE
+            && tag(nextSibling) === tag(lastChild)) {
             nextSibling.innerHTML = lastChild.innerHTML + nextSibling.innerHTML;
             lastChild.remove();
+          }
+          
+          span.outerHTML = span.innerHTML; 
         }
-        
-        span.outerHTML = span.innerHTML;
+        alsoRemove.forEach(this.removeHighlightSpan);
       },
       createHighlight(contents) {
-          const span = this.createHighlightSpan(contents);
-          span.childNodes.forEach((value, key) => {
-            if (value.className == highlightClass) {
-              value.replaceWith(value.innerHTML);
-            }
-          });
-          return span;
+        const span = this.createHighlightSpan(contents);
+        span.childNodes.forEach((value, key) => {
+          if (value.className == highlightClass) {
+            value.replaceWith(value.innerHTML);
+          }
+        });
+        return span;
       },
       createHighlightSpan(contents) {
         if (!contents) {
@@ -173,13 +217,13 @@ function Highlightable(rootElementSelector) {
         if (typeof contents === 'string') {
           span.innerHTML = contents;
         } else if (contents instanceof DocumentFragment) {
-            span.appendChild(contents);
+          span.appendChild(contents);
         } else if (contents.style) {
           span = contents;
         } else {
           return null;
         }
-        span.style.backgroundColor = "yellow";
+        // span.style.backgroundColor = "yellow";
         span.classList.add(highlightClass);
         span.onclick = this.onClick;
 
@@ -199,42 +243,42 @@ function Highlightable(rootElementSelector) {
           //Let's make sure we get its connected spans in cases where a highlight overflows into consecutive paragraph(s)
           if (currEl.classList.contains(overflowPrevClass) || currEl.classList.contains(overflowNextClass)) {
             //Treating a JavaScript array as a double-sided queue allows us to efficiently traverse above and below the clicked-on element to find the full flow
-              let deque = [currEl];
-              while (deque[0].classList.contains(overflowPrevClass)) {
-                  let prevEl = deque[0].previousElementSibling;
-                  if (prevEl == null) {
-                    prevEl = deque[0].parentElement.previousElementSibling;
-                  }
-                  if (prevEl.classList.contains(highlightClass)) {
-                      deque.unshift(prevEl);
-                  } else if (prevEl.lastElementChild.classList.contains(highlightClass)) {
-                      deque.unshift(prevEl.lastElementChild);
-                  }
+            let deque = [currEl];
+            while (deque[0].classList.contains(overflowPrevClass)) {
+              let prevEl = deque[0].previousElementSibling;
+              if (prevEl == null) {
+                prevEl = deque[0].parentElement.previousElementSibling;
               }
-              while (deque[deque.length - 1].classList.contains(overflowNextClass)) {
-                let nextEl = deque[deque.length - 1].nextElementSibling;
-                if (nextEl == null) {
-                  nextEl = deque[deque.length - 1].parentElement.nextElementSibling;
-                }
-                if (nextEl.classList.contains(highlightClass)) {
-                  deque.push(nextEl);
-                } else if (nextEl.firstElementChild.classList.contains(highlightClass)) {
-                  deque.push(nextEl.firstElementChild);
-                }
+              if (prevEl.classList.contains(highlightClass)) {
+                deque.unshift(prevEl);
+              } else if (prevEl.lastElementChild.classList.contains(highlightClass)) {
+                deque.unshift(prevEl.lastElementChild);
               }
+            }
+            while (deque[deque.length - 1].classList.contains(overflowNextClass)) {
+              let nextEl = deque[deque.length - 1].nextElementSibling;
+              if (nextEl == null) {
+                nextEl = deque[deque.length - 1].parentElement.nextElementSibling;
+              }
+              if (nextEl.classList.contains(highlightClass)) {
+                deque.push(nextEl);
+              } else if (nextEl.firstElementChild.classList.contains(highlightClass)) {
+                deque.push(nextEl.firstElementChild);
+              }
+            }
 
-              metadata = deque[0].dataset.rangeData;
-              html = deque
-                .map(strippedAttributes)
-                .map(el => el.outerHTML) //grab the element's html
-                .join(' ');
+            metadata = deque[0].dataset.rangeData;
+            html = deque
+              .map(strippedAttributes)
+              .map(el => el.outerHTML) //grab the element's html
+              .join(' ');
           } else {
             metadata = currEl.dataset.rangeData;
             html = strippedAttributes(currEl).outerHTML;
           }
           event.dataTransfer.setData("metadata", metadata);
           event.dataTransfer.setData("text/html", html);
-          function strippedAttributes (el) {
+          function strippedAttributes(el) {
             const clone = el.cloneNode(true);
             Array.from(clone.attributes).forEach(attr => clone.removeAttribute(attr.name));
             return clone;
@@ -268,36 +312,36 @@ function Highlightable(rootElementSelector) {
         return `${startPath}-${range.startOffset};${endPath}-${range.endOffset}`
       },
       deserializeRange(string) {
-          const startEnd = string.split(';');
-          const startPath = startEnd[0].split(/\/|-/g);
-          const endPath =   startEnd[1].split(/\/|-/g);
-          const startOffset = startPath.pop();
-          const endOffset = endPath.pop();
+        const startEnd = string.split(';');
+        const startPath = startEnd[0].split(/\/|-/g);
+        const endPath = startEnd[1].split(/\/|-/g);
+        const startOffset = startPath.pop();
+        const endOffset = endPath.pop();
 
-          const pathToNode = path =>
-            path.slice(1).reduce((prev, curr) => {
-              const split = curr.split(/\[|\]/g);
-              const name = split[0];
-              const index = split[1];
-              const child = prev.childNodes[index];
-              if (!child) {
-                console.log("ERROR HERE");
-                console.log(prev);
-                console.log(prev.childNodes)
-                console.log(curr);
-              }
-              if (child.nodeName.toUpperCase() !== name.toUpperCase()) {
-                console.log(curr);
-                console.dir(prev);
-                console.warn(`Element ${curr} has changed since the last highlight`);
-              }
-              return child;
-            }, document.querySelector(path[0]))
+        const pathToNode = path =>
+          path.slice(1).reduce((prev, curr) => {
+            const split = curr.split(/\[|\]/g);
+            const name = split[0];
+            const index = split[1];
+            const child = prev.childNodes[index];
+            if (!child) {
+              console.log("ERROR HERE");
+              console.log(prev);
+              console.log(prev.childNodes)
+              console.log(curr);
+            }
+            if (child.nodeName.toUpperCase() !== name.toUpperCase()) {
+              console.log(curr);
+              console.dir(prev);
+              console.warn(`Element ${curr} has changed since the last highlight`);
+            }
+            return child;
+          }, document.querySelector(path[0]))
 
-          const range = new Range();
-          range.setStart(pathToNode(startPath), startOffset);
-          range.setEnd(pathToNode(endPath), endOffset);
-          return range;
+        const range = new Range();
+        range.setStart(pathToNode(startPath), startOffset);
+        range.setEnd(pathToNode(endPath), endOffset);
+        return range;
       }
     }
   }
