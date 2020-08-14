55bbfc086f370dad9467d6ccba0bf9889dabf380
Dan Jutan <danjutan@gmail.com>
2020-08-14 19:49:58 -0400

    get rid of double timeline bug in debugging

--- client/src/noteableVisualizations.js
+++ client/src/noteableVisualizations.js
@@ -1,4 +1,4 @@
-import PeabodyGrid from "./components/vis/peabody/OLDPeabodyGrid";
+import PeabodyGrid from "./components/vis/peabody/newpeabodygrid/PeabodyGrid";
 import PeabodyMutable from "./components/vis/peabody/PeabodyMutable";
 import MapScroller from "./components/scrollytelling/MapScroller";
 
