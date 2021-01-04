d4ab1e1d0e2bb7be50613ab265f9a17cd90fd60f
Lauren Klein <lklein@gmail.com>
2021-01-03 22:33:50 -0500

    Tweak caption of covid/income/women vis

--- client/src/components/vis/playfair/creationProcess/DataSetsVis.vue
+++ client/src/components/vis/playfair/creationProcess/DataSetsVis.vue
@@ -3,9 +3,9 @@
     <D3ImplCovid v-if="idx == 0"/>
     <D3ImplIncome v-if="idx == 1"/>
     <D3ImplWomen v-if="idx == 2"/>
-    <div style="text-align: center">
-    <a class="blue-hover" @mouseover="idx = 0"> Covid-19 Deaths </a>
-    <a class="blue-hover" @mouseover="idx = 1"> Average Income </a>
+    <div style="text-align: center; margin-top: 10px">
+    <a class="blue-hover" @mouseover="idx = 0"> Covid-19 Deaths </a> &bull;
+    <a class="blue-hover" @mouseover="idx = 1"> Average Income </a> &bull;
     <a class="blue-hover" @mouseover="idx = 2"> Women Representatives </a>
     </div>
     <div style="text-align: center; font-family: Consolas; font-size: 90%; margin-top: 10px">Three contemporary datasets visualized in the style of William Playfair.</div>
