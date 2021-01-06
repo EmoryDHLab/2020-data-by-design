f4abaceb9230eb77213583cfe01fc17556404d91
Dan Jutan <danjutan@gmail.com>
2021-01-05 20:39:29 -0500

    fix offset accuracy; add pause feature

--- client/src/components/waypoints/BasicWaypoint.vue
+++ client/src/components/waypoints/BasicWaypoint.vue
@@ -46,39 +46,49 @@ export default {
      */
     disable () {
       this.waypoint.disable()
+    },
+    destroyWaypoint () {
+      if (this.waypoint && this.waypoint.destroy) {
+        this.waypoint.destroy();
+      }
+    },
+    createWaypoint () {
+      this.destroyWaypoint();
+      const vm = this;
+      // set up the waypoint config
+      const context = document.getElementById(this.contextId) || window;
+      const conf = {
+        // attach to this element
+        element: this.$el,
+        // set the offset (from top) where the waypoint will trigger
+        offset: this.offset,
+        // set whether the waypoint is active (it won't emit events if it is off)
+        enabled: this.enabled,
+        // context,
+        handler: (direction) => {
+          // emit a 'triggered' event no mater what
+          vm.$emit('triggered')
+          if (direction) {
+            // emit triggered:up or triggered:down depending on direction
+            vm.$emit(`triggered:${direction}`)
+          }
+        },
+        scrollHandler: ({newScroll, oldScroll, direction, lastWaypoint, nextWaypoint}) => {
+          vm.$emit('scrolled', {newScroll, oldScroll, direction, lastWaypoint, nextWaypoint});
+        }
+      }
+      // actually create the waypoint (store it in component data)
+      const waypoint = new window.Waypoint(conf);
+      console.log("created waypoint");
+      Object.assign(this.waypoint, {}, waypoint)
     }
   },
   mounted () {
-    const vm = this;
-    // set up the waypoint config
-    const context = document.getElementById(this.contextId) || window;
-    const conf = {
-      // attach to this element
-      element: this.$el,
-      // set the offset (from top) where the waypoint will trigger
-      offset: this.offset,
-      // set whether the waypoint is active (it won't emit events if it is off)
-      enabled: this.enabled,
-      // context,
-      handler: (direction) => {
-        // emit a 'triggered' event no mater what
-        vm.$emit('triggered')
-        if (direction) {
-          // emit triggered:up or triggered:down depending on direction
-          vm.$emit(`triggered:${direction}`)
-        }
-      },
-      scrollHandler: ({newScroll, oldScroll, direction, lastWaypoint, nextWaypoint}) => {
-        vm.$emit('scrolled', {newScroll, oldScroll, direction, lastWaypoint, nextWaypoint});
-      }
-    }
-    // actually create the waypoint (store it in component data)
-    const waypoint = new window.Waypoint(conf);
-    Object.assign(this.waypoint, {}, waypoint)
+    setTimeout(this.createWaypoint, 800);
   },
   beforeDestroy () {
     // make sure we destroy the waypoint before the component is destroyed
-    if (this.waypoint) this.waypoint.destroy()
+    this.destroyWaypoint();
   },
   watch: {
     enabled (newVal, oldVal) {
@@ -89,7 +99,7 @@ export default {
       }
     },
     offset (newVal, oldVal) {
-      this.waypoint.options.offset = newVal;
+      // this.createWaypoint();
     }
   }
 }
