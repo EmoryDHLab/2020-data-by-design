cb428e7378551e1403bfc4034008bbc7128feab3
Lauren Klein <lklein@gmail.com>
2021-05-26 11:14:16 -0400

    Updated images/vis placements in Du Bois chapter

--- client/src/views/DuboisChapter.vue
+++ client/src/views/DuboisChapter.vue
@@ -1,10 +1,58 @@
 <template lang="html">
   <chapter-scaffold v-bind:curChapter="'Dubois'"
                     docId="1fnhR4aWJ7oNR8GHljHtKggNFS70CfuLSlMxO8rxp4bc">
-
+      <template slot='title'>
+        Between Data and Truth: W.E.B. Du Bois and the Limits of Visual Display
+      </template>
     <template v-slot:default="{ docRendererProps }">
       <DocRenderer v-bind="docRendererProps">
+        <!--Inline Slots-->
 
+        <template v-slot:StatisticalAtlas>
+          <div class="IMG">
+            <div class="grid-row">
+              <img class="grid-image" src="./img/ch5-06a-letter.jpg"
+                         width="40%"/>
+              <img class="grid-image" src="./img/ch5-06b-map.jpg"
+                         width="40%"/>
+            </div>
+            <p class="caption">
+              The TKTK (left), and the TKTK (right).
+            </p>
+          </div>
+        </template>
+
+        <template v-slot:AtlasComparison>
+          <div class="IMG">
+            <div class="grid-row">
+              <img class="grid-image" src="./img/ch5-08a-population.jpg" width="40%"/>
+              <img class="grid-image" src="./img/ch5-08b-amalg.jpg" width="40%"/>
+            </div>
+            <div class="grid-row">
+              <img class="grid-image" src="./img/ch5-08c-conjugal.jpg" width="40%"></img>
+              <img class="grid-image" src="./img/ch5-08d-db-conjugal.jpg" width="40%"></img>
+            </div>
+            <p class="caption">
+                    Caption TK.
+            </p>
+          </div>
+        </template>
+
+        <template v-slot:ChalabiOnuoha>
+          <div class="IMG">
+            <div class="grid-row">
+              <img class="grid-image" src="./img/ch5-16a-chalabi.jpg" width="100%"/>
+            </div>
+            <div class="grid-row">
+              <img class="grid-image" src="./img/ch5-17-onuoha.jpg" width="100%"></img>
+            </div>
+            <p class="caption">
+                    Caption TK.
+            </p>
+          </div>
+        </template>
+
+        <!-- Component Slots -->
         <template v-slot:[slots.dubois_recreation_set]>
           <DuboisRecreationSet key="1"/>
         </template>
