1522d30d8d7de80a13e6cd37421ec4ba046b07f7
Dan Jutan <danjutan@gmail.com>
2021-11-07 03:17:09 +0000

    work with env variables instead of docsconfig

--- server-express/routes/api/doc.js
+++ server-express/routes/api/doc.js
@@ -1,27 +1,23 @@
 const router = require("express").Router();
 const parseDocs = require('parse-google-docs-json');
 
-try {
-  const conf = require("../../docsconfig.json");
-  router.get("/:id", async (req, res, next) => {
-    const documentId = req.params.id;
-    try {
-      const parsed = await parseDocs({
-        documentId,
-        clientEmail: conf.client_email,
-        privateKey: conf.private_key
-      });
-      console.dir(parsed);
-      res.json(parsed.toJson());
-    } catch (error) {
-      next(error);
-    }
-  });
-} catch (e) {
-  if (e.code == 'MODULE_NOT_FOUND') {
-    console.error("The docsconfig.json file is missing. In order for the /doc API endpoint to work, you'll need to provide this API credentials file and put it in the server root (not the project root!), see here: https://flaviocopes.com/google-api-authentication/")
+const private_key = process.env.PRIVATE_KEY_64 ? Buffer.from(process.env.PRIVATE_KEY_64, "base64") : process.env.PRIVATE_KEY;
+
+
+router.get("/:id", async (req, res, next) => {
+  const documentId = req.params.id;
+  try {
+    const parsed = await parseDocs({
+      documentId,
+      clientEmail: process.env.CLIENT_EMAIL,
+      privateKey: private_key
+    });
+    console.dir(parsed);
+    res.json(parsed.toJson());
+  } catch (error) {
+    next(error);
   }
-}
+});
 
 
 module.exports = router;
